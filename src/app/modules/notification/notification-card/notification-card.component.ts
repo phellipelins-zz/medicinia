d7c47c1f412aa37146e3bb5a0fe336665af0cf73
Phellipe Lins <phellipelins@gmail.com>
2017-12-29 08:00:15 -0300

    display patient's name on card

--- src/app/modules/notification/notification-card/notification-card.component.ts
+++ src/app/modules/notification/notification-card/notification-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { PatientService } from '../../../services/patient/patient.service';
+import { Patient } from '../../patient/patient';
+import { Notification } from '../../notification/notification';
 
 @Component({
   selector: 'notification-card',
@@ -8,13 +11,18 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class NotificationCardComponent implements OnInit {
   @Input() notification: Notification;
   @Output() onPatientChange = new EventEmitter<any>();
+  private patient: Patient = new Patient('...', new Date(), 'female', '', {});
   typeTranslation: any = { 'consultation': 'Consulta',
                            'exam': 'Exame',
                            'surgery': 'Cirurgia' };
 
-  constructor() { }
+  constructor(protected patientService: PatientService) { }
 
   ngOnInit() {
+    this.patientService.getById(this.notification.patient_id).subscribe(
+      response => this.patient = response || this.patient,
+      err => console.error(err)
+    )
   }
 
   updatePatient(patient_id) {
