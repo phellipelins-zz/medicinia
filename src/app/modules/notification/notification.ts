d389e88d6e8e587341bdf1611dff560c8cdbfb24
Phellipe Lins <phellipelins@gmail.com>
2017-12-28 22:19:48 -0300

    Notification Class

---
+++ src/app/modules/notification/notification.ts
@@ -0,0 +1,10 @@
+export class Notification {
+  content: String;
+  image_url: String;
+  isArchived: Boolean;
+  isFavorite: Boolean;
+  patient_id: String;
+  scheduled_date: Date;
+  type: String;
+  _id: String;
+}
