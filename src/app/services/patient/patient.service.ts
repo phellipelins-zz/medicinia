fe082fe43e63d6d768108374dff64c852386f6d7
Phellipe Lins <phellipelins@gmail.com>
2017-12-28 22:21:56 -0300

    PatientService to get our patients data

---
+++ src/app/services/patient/patient.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs/Observable';
+
+const API_URL = `${environment.apiUrl}/patients`;
+
+@Injectable()
+export class PatientService {
+  constructor(protected http: HttpClient) { }
+  
+  public getAll(): Observable<any[]> {
+    return this.http.get<any[]>(API_URL)
+  }
+
+  public getById(id): Observable<any> {
+    return this.http.get<any>(`${API_URL}/${id}`)
+  }
+}
